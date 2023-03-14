7e3521c53f2944d295c452ec65c62fc8f9f159dc
Anatoliy-Usov <109044779+Anatoliy-Usov@users.noreply.github.com>
2023-03-14 15:30:04 +0000

    Deploying to gh-pages from @ Anatoliy-Usov/goit-js-hw-09@22a0f687253c4f66ded8d9d33daa2f8265fc553c 🚀

---
+++ 01-color-switcher.84cb3a3a.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");let o=0;t.addEventListener("click",(function(){t.setAttribute("disabled","true"),o=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),500),e.removeAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(o),t.removeAttribute("disabled"),e.setAttribute("disabled","true")}));
+//# sourceMappingURL=01-color-switcher.84cb3a3a.js.map
