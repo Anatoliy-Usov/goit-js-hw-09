afa4406f56b294f892df5468949d2b912d8fc621
Anatoliy-Usov <109044779+Anatoliy-Usov@users.noreply.github.com>
2023-03-16 16:23:48 +0000

    Deploying to gh-pages from @ Anatoliy-Usov/goit-js-hw-09@19ee3ff27de29393b615a013aea0a8a222f45b83 🚀

--- 01-color-switcher.fc8867dc.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body"),o=0;t.addEventListener("click",(function(){t.setAttribute("disabled","true"),o=setInterval((function(){r.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),500),e.removeAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(o),t.removeAttribute("disabled"),e.setAttribute("disabled","true")}))}();
-//# sourceMappingURL=01-color-switcher.fc8867dc.js.map
